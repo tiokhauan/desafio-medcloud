becaf9174cf1aadf79a07b3bbb5c7be7210dfbfe
tiokhauan <khauan71@gmail.com>
2024-10-27 15:54:34 -0300

    erro formatdate

--- src/budgets.tf
+++ src/budgets.tf
@@ -1,11 +1,11 @@
 resource "aws_budgets_budget" "monthly_cost_budget" {
   name              = "MonthlyCostBudget"
   budget_type       = "COST"
-  limit_amount      = "1"                  # Valor máximo do orçamento em dólares
+  limit_amount      = "100"                  # Valor máximo do orçamento em dólares
   limit_unit        = "USD"
 
-  # Definindo time_period_start diretamente como uma string
-  time_period_start = "2024-10-01T00:00:00Z"  # O primeiro dia do mês atual
+  # Formatando a data sem "T" e "Z"
+  time_period_start = formatdate("2006-01-02_15:04", timestamp())  # Formato sem T e Z
   time_unit         = "MONTHLY"
 
   # Notificação quando o custo atingir 80% do limite
