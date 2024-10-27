87b86595fecd844681b187c98f30f09b74451e41
tiokhauan <khauan71@gmail.com>
2024-10-27 15:45:30 -0300

    ajuste formato de data e hora

--- src/budgets.tf
+++ src/budgets.tf
@@ -1,17 +1,18 @@
 resource "aws_budgets_budget" "monthly_cost_budget" {
   name              = "MonthlyCostBudget"
   budget_type       = "COST"
-  limit_amount      = "1"
+  limit_amount      = "1"                  # Valor máximo do orçamento em dólares
   limit_unit        = "USD"
   
-  time_period_start = formatdate("2006-01-02T15:04:05Z", timestamp())
+  # Corrigindo o formato da data
+  time_period_start = formatdate("2006-01-02 15:04:05", timestamp())
   time_unit         = "MONTHLY"
 
   # Notificação quando o custo atingir 80% do limite
   notification {
     comparison_operator = "GREATER_THAN"
     notification_type   = "ACTUAL"
-    threshold           = 80
+    threshold           = 80                 # Percentual do orçamento para a notificação
     threshold_type      = "PERCENTAGE"
     subscriber_email_addresses = ["khauan71@gmail.com"]
   }
